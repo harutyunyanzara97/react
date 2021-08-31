f394e58840dd114800ee0d109d2d6368f280b790
harutyunyanzara97 <harutyunyanzara97@gmail.com>
2021-08-31 11:02:35 +0400

    Single page applicaions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,31 +1,47 @@
 import React,{Component} from 'react'
-import Menu from './MenuComponent';
-import Dish from './DishdetailComponent'
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import {Switch, Route, Redirect, Router} from 'react-router-dom';
+
 
 import {DISHES} from "../shared/dishes";
 
 class Main extends Component {
     constructor(props){
         super(props);
-        this.state= {
-            dishes:DISHES,
-            selectedDish:null
+        this.state = {
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
+    }
 
     }
 
-    onDishSelect(dishId) {
-        this.setState({selectedDish:dishId});
+    const HomePage = () => {
+        return(
+            <Home
+                dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+            />
+        );
     }
-    render() {
         return (
             <div>
                 <Header />
-                <Menu dishes={this.state.dishes} />
-                onClick={(dishId) => this.onDishSelect(dishId)}
-                <Dish dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+              <Switch>
+                  <Route path="/home" component={HomePage}/>
+                  <Route exact path="/menu" component={() => <Menu dishes = {this.state.dishes}/>}></Route>
+                  <Redirect to={"/home"}></Redirect>
+                  <Route exact path='/contactus' component={Contact} />} />
+              </Switch>
                 <Footer />
             </div>
         // <Navbar dark color="primary">
@@ -34,9 +50,9 @@ class Main extends Component {
         //     </div>
         // </Navbar>
         );
-    }
 
-}
+
+
 
 
 export default Main;
--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -1,215 +1,43 @@
-export const DISHES = [
-    {
-        id: 0,
-        name:'Uthappizza',
-        image: 'assets/images/uthappizza.png',
-        category: 'mains',
-        label:'Hot',
-        price:'4.99',
-        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-        comments: [
-            {
-                id: 0,
-                rating: 1,
-                comment: "How you doin'?",
-                author: "Joey Tribbiani",
-                date: "2012-10-16T17:57:28.556094Z"
-            },
-            {
-                id: 1,
-                rating: 5,
-                comment: "Shut up, shut up, shut up!",
-                author: "Chandler Bing",
-                date: "2014-09-05T17:57:28.556094Z"
-            },
-            {
-                id: 2,
-                rating: 5,
-                comment: "Smelly Cat, Smelly Cat",
-                author: "Phoebe Buffay",
-                date: "2015-02-13T17:57:28.556094Z"
-            },
-            {
-                id: 3,
-                rating: 5,
-                comment: "You feel Asleeep?!",
-                author: "Rachel Greene",
-                date: "2013-12-02T17:57:28.556094Z"
-            },
-            {
-                id: 4,
-                rating: 5,
-                comment: "Welcome to the real world. It sucks. You’re gonna love it",
-                author: "Monica Geller",
-                date: "2011-12-02T17:57:28.556094Z"
-            },
-            {
-                id: 4,
-                rating: 5,
-                comment: "We were on a break!",
-                author: "Ross Geller",
-                date: "2011-12-02T17:57:28.556094Z"
-            }
-        ]
-    },
-
-    {
-        id: 1,
-        name:'Zucchipakoda',
-        image: 'assets/images/zucchipakoda.png',
-        category: 'appetizer',
-        label:'',
-        price:'1.99',
-        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-        comments: [
-            {
-                id: 0,
-                rating: 1,
-                comment: "How you doin'?",
-                author: "Joey Tribbiani",
-                date: "2012-10-16T17:57:28.556094Z"
-            },
-            {
-                id: 1,
-                rating: 5,
-                comment: "Shut up, shut up, shut up!",
-                author: "Chandler Bing",
-                date: "2014-09-05T17:57:28.556094Z"
-            },
-            {
-                id: 2,
-                rating: 5,
-                comment: "Smelly Cat, Smelly Cat",
-                author: "Phoebe Buffay",
-                date: "2015-02-13T17:57:28.556094Z"
-            },
-            {
-                id: 3,
-                rating: 5,
-                comment: "You feel Asleeep?!",
-                author: "Rachel Greene",
-                date: "2013-12-02T17:57:28.556094Z"
-            },
-            {
-                id: 4,
-                rating: 5,
-                comment: "Welcome to the real world. It sucks. You’re gonna love it",
-                author: "Monica Geller",
-                date: "2011-12-02T17:57:28.556094Z"
-            },
-            {
-                id: 4,
-                rating: 5,
-                comment: "We were on a break!",
-                author: "Ross Geller",
-                date: "2011-12-02T17:57:28.556094Z"
-            }
-        ]
-    },
-    {
-        id: 2,
-        name:'Vadonut',
-        image: 'assets/images/vadonut.png',
-        category: 'appetizer',
-        label:'New',
-        price:'1.99',
-        description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
-        comments: [
-            {
-                id: 0,
-                rating: 1,
-                comment: "How you doin'?",
-                author: "Joey Tribbiani",
-                date: "2012-10-16T17:57:28.556094Z"
-            },
-            {
-                id: 1,
-                rating: 5,
-                comment: "Shut up, shut up, shut up!",
-                author: "Chandler Bing",
-                date: "2014-09-05T17:57:28.556094Z"
-            },
-            {
-                id: 2,
-                rating: 5,
-                comment: "Smelly Cat, Smelly Cat",
-                author: "Phoebe Buffay",
-                date: "2015-02-13T17:57:28.556094Z"
-            },
-            {
-                id: 3,
-                rating: 5,
-                comment: "You feel Asleeep?!",
-                author: "Rachel Greene",
-                date: "2013-12-02T17:57:28.556094Z"
-            },
-            {
-                id: 4,
-                rating: 5,
-                comment: "Welcome to the real world. It sucks. You’re gonna love it",
-                author: "Monica Geller",
-                date: "2011-12-02T17:57:28.556094Z"
-            },
-            {
-                id: 4,
-                rating: 5,
-                comment: "We were on a break!",
-                author: "Ross Geller",
-                date: "2011-12-02T17:57:28.556094Z"
-            }
-        ]
-    },
-    {
-        id: 3,
-        name:'ElaiCheese Cake',
-        image: 'assets/images/elaicheesecake.png',
-        category: 'dessert',
-        label:'',
-        price:'2.99',
-        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-        comments: [
-            {
-                id: 0,
-                rating: 1,
-                comment: "How you doin'?",
-                author: "Joey Tribbiani",
-                date: "2012-10-16T17:57:28.556094Z"
-            },
-            {
-                id: 1,
-                rating: 5,
-                comment: "Shut up, shut up, shut up!",
-                author: "Chandler Bing",
-                date: "2014-09-05T17:57:28.556094Z"
-            },
-            {
-                id: 2,
-                rating: 5,
-                comment: "Smelly Cat, Smelly Cat",
-                author: "Phoebe Buffay",
-                date: "2015-02-13T17:57:28.556094Z"
-            },
-            {
-                id: 3,
-                rating: 5,
-                comment: "You feel Asleeep?!",
-                author: "Rachel Greene",
-                date: "2013-12-02T17:57:28.556094Z"
-            },
-            {
-                id: 4,
-                rating: 5,
-                comment: "Welcome to the real world. It sucks. You’re gonna love it",
-                author: "Monica Geller",
-                date: "2011-12-02T17:57:28.556094Z"
-            },
-            {
-                id: 4,
-                rating: 5,
-                comment: "We were on a break!",
-                author: "Ross Geller",
-                date: "2011-12-02T17:57:28.556094Z"
-            }
-        ]
-    }
-];
+export const DISHES =
+    [
+        {
+            id: 0,
+            name:'Uthappizza',
+            image: '/assets/images/uthappizza.png',
+            category: 'mains',
+            label:'Hot',
+            price:'4.99',
+            featured: true,
+            description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+        },
+        {
+            id: 1,
+            name:'Zucchipakoda',
+            image: '/assets/images/zucchipakoda.png',
+            category: 'appetizer',
+            label:'',
+            price:'1.99',
+            featured: false,
+            description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+        },
+        {
+            id: 2,
+            name:'Vadonut',
+            image: '/assets/images/vadonut.png',
+            category: 'appetizer',
+            label:'New',
+            price:'1.99',
+            featured: false,
+            description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
+        },
+        {
+            id: 3,
+            name:'ElaiCheese Cake',
+            image: '/assets/images/elaicheesecake.png',
+            category: 'dessert',
+            label:'',
+            price:'2.99',
+            featured: false,
+            description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+        }
+    ];
